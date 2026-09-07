2b90620483aaaed2c18b133b5c10070fb25d87be
Nick Kennedy <nick@nick-kennedy.com>
2026-09-07 10:17:32 +0100

    Pass config entry ID to device registry lookup

--- tests/test_services.py
+++ tests/test_services.py
@@ -122,7 +122,9 @@ async def test_charge_now_service_targets_device(hass, bypass_get_data):
     await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
 
-    device = dr.async_get(hass).async_get_device_by_identifier((DOMAIN, "PSL-123456"))
+    device = dr.async_get(hass).async_get_device_by_identifier(
+        (DOMAIN, "PSL-123456"), config_entry.entry_id
+    )
     assert device is not None
 
     with patch(
